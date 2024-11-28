5d314b5f963cd83b1d33f5d310da090e515d8cda
Mawais200 <fa22-bsse-201@lgu.edu.pk>
2024-11-28 09:36:38 -0800

    Update bootsrap.js
    this is an updated Javascript code.

--- bootsrap.js
+++ bootsrap.js
@@ -1,32 +1,32 @@
-var carouselWidth = $('.carousel-inner')[0].scrollWidth;
-var cardWidth = $('.carousel-item').width();
 
-// Initialize the scroll position to be half the width of a card
-var scrollPosition = cardWidth / 2;
-
-// Next button click event handler
-$('.carousel-control-next').on('click', function () {
-    // Calculate the new scroll position
-    scrollPosition += cardWidth;
-
-    // Animate the carousel scroll
-    $('.carousel-inner').animate({
-        scrollLeft: scrollPosition
-    }, 600);
-});
-
-// Prev button click event handler
-$('.carousel-control-prev').on('click', function () {
-    // Calculate the new scroll position
-    scrollPosition -= cardWidth;
-
-    // Ensure the scroll position doesn't go below 0
-    if (scrollPosition < cardWidth / 2) {
-        scrollPosition = cardWidth / 2;
-    }
-
-    // Animate the carousel scroll
-    $('.carousel-inner').animate({
-        scrollLeft: scrollPosition
-    }, 600);
+$('.center').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 2000, // 2000ms = 2s
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1,
+                autoplay: true,
+                autoplaySpeed: 2000,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1,
+                autoplay: true,
+                autoplaySpeed: 2000,
+            }
+        }
+    ]
 });
